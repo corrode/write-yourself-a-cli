b50f0e7880048c23e836e9c8f1a1b543c0d700c8
Matthias <matthias@endler.dev>
2025-04-09 09:38:37 +0200

    Clean up main entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-use std::{
-    fs::{self},
-    path::PathBuf,
-    str::FromStr,
-};
+use std::{path::PathBuf, str::FromStr};
+
 fn iter_files(path: PathBuf) {
     // Your code here
 }
+
 fn main() {
     iter_files(PathBuf::from_str(".").unwrap());
 }
